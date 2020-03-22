bac8ce16c36f04546836ab49caadbdc8be528443
BugSurvivor <50827056+BugSurvivor@users.noreply.github.com>
2020-03-22 14:32:23 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git config --global user.name jiandahao
 git config --global user.email jiandahao@gmail.com
 mkdir /blog && cd /blog
 hexo init
+npm install --save hexo-deployer-git
 cp /data/_config.yml .
 cp -a /data/* source/_posts/
 hexo generate && hexo deploy
